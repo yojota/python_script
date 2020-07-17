cfe4b3670f36758512ed3474ce784059a06acb44
Jota Perez <yojota@gmail.com>
2020-07-17 10:52:02 -0300

    add script for validate email

---
+++ multi_validate_email.py
@@ -0,0 +1,39 @@
+from verify_email import verify_email, verify_email_async
+import time
+import asyncio
+
+
+emails = [
+    'k.akshay9721@gmail.com',
+    'some.email.address.that.does.not.exist@gmail.com',
+    'foo@bar.com',
+    'ex@example.com'
+]
+
+def time_it(func, *args, **kwargs):
+    start = time.time()
+    func(*args, **kwargs)
+    print('used:', time.time() - start, 'seconds')
+
+
+def single(email):
+    print(email, 'is', verify_email(email) and 'valid' or 'invalid')
+
+
+def multiple():
+    print('\t'.join(emails))
+    print('\t'.join(i and 'valid' or 'invalid' for i in verify_email(emails)))
+
+async def main():
+    print('\t'.join(emails))
+    print('\t'.join(i and 'valid' or 'invalid' for i in await verify_email_async(emails)))
+
+
+print('### single')
+for email in emails:
+    time_it(single, email)
+print('### multiple')
+time_it(multiple)
+print('note that network condition may have impact on test results')
+print("asyncio test")
+asyncio.get_event_loop().run_until_complete(main())
---
+++ validate_email.py
@@ -0,0 +1,11 @@
+#requerimientos
+#Python 3.7
+#sudo apt-get install python3-pip 
+#install library
+#sudo pip3 install verify-email
+
+from verify_email import verify_email
+
+validacion = verify_email('juan.perez@nicromit.com')
+
+print (validacion)
